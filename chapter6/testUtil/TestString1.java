66a6c195f90168909cabe166a6151ce28f7f65f5
yinzhiying <457365826@qq.com>
2014-09-30 08:47:35 +0800

    Update TestString1.java

--- chapter6/testUtil/TestString1.java
+++ chapter6/testUtil/TestString1.java
@@ -10,7 +10,7 @@ public class TestString1{
 		String s = scan.nextLine();
 		
 		for(int i = 0;i<s.length()/2;i++){
-			if(s.charAt(i) !== s.charAt(s.length()-(i+1))){
+			if(s.charAt(i) != s.charAt(s.length()-(i+1))){
 				flag = false;
 			}
 		}
